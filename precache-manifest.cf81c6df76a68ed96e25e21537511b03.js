0bc570d5510dbf2593d3ffa41ce2746db88e5f93
Priyanka Tiwari <priyankatiwary23@gmail.com>
2020-09-14 23:09:55 +0530

    Updates

---
+++ precache-manifest.cf81c6df76a68ed96e25e21537511b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8080c1d95b4dc748dc5ba809986fc7b9",
+    "url": "/Movieflix/index.html"
+  },
+  {
+    "revision": "11305e8bbbf623db510e",
+    "url": "/Movieflix/static/css/main.8fb5447f.chunk.css"
+  },
+  {
+    "revision": "5aa2b0ebbcffefdc70dc",
+    "url": "/Movieflix/static/js/2.bb7944bf.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Movieflix/static/js/2.bb7944bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11305e8bbbf623db510e",
+    "url": "/Movieflix/static/js/main.d921d555.chunk.js"
+  },
+  {
+    "revision": "8c0027e5b987799e5581",
+    "url": "/Movieflix/static/js/runtime-main.42e67dc4.js"
+  }
+]);
